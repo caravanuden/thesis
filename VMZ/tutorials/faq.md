11d22545760c5ceadeffde64e5daf74dd830958a
Cara Van Uden <Cara.E.Van.Uden.19@dartmouth.edu>
2019-04-15 15:28:22 -0400

    removed VMZ dir from this repo (already on github) and updated name of docker_r21d_dependencies.sh --> thesis_dependencies.sh (more accurate name)

--- VMZ/tutorials/faq.md
+++
@@ -1,9 +0,0 @@
-# FAQ / Troubleshooting Help
-
-1. [Caffe2 FAQ](https://caffe2.ai/docs/faq.html)
-2. [ImportError: No module named models.model_builder](#ImportError-No-module-named-models.model_builder)
-
-
-
-### ImportError: No module named models.model_builder
-At root directory(R2Plus1D), do: ```export PYTHONPATH=$PYTHONPATH:`pwd`/lib```
--- neurodocker_recipes.sh
+++ neurodocker_recipes.sh
@@ -2,8 +2,8 @@
 echo "docker Dockerfile
 singularity Singularity" | while read backend filename; do
     docker run --rm kaczmarj/neurodocker:master generate $backend  \
-      -b centos -p yum --copy docker_r21d_dependencies.sh / \
-      -r /docker_r21d_dependencies.sh \
+      -b centos -p yum --copy thesis_dependencies.sh / \
+      -r /thesis_dependencies.sh \
       -e PYTHONPATH=/usr/local/lib/python2.7/site-packages/  LD_LIBRARY_PATH=/usr/local/lib PKG_CONFIG_PATH=/usr/local/lib/pkgconfig \
       | sed -e 's,/tmp/\* .*,,g' >| $filename
 done
