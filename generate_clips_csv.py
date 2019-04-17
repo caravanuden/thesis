0c5dafef2e806ef2d326077b9bd37ed30427bc81
Cara Van Uden <Cara.E.Van.Uden.19@dartmouth.edu>
2019-04-17 13:21:01 -0400

    testing on part 1 of raiders only

--- generate_clips_csv.py
+++ generate_clips_csv.py
@@ -2,7 +2,7 @@ import cv2, csv
 
 DATA_DIR = "./raiders/stimuli/task002/scaled_cropped/"
 CSV_FILE = "raiders_clips.csv"
-STRIDE_LEN = 32
+STRIDE_LEN = 8
 
 # def get_vid_length(input_video):
 #     print(input_video)
@@ -20,7 +20,7 @@ with open(CSV_FILE, mode="w") as f:
     # write header
     f.write("org_video,label,start_frm,video_id\n")
     id = 0
-    for i in range(1,9):
+    for i in range(1,2):
         video = DATA_DIR + "INDIANA_JONES_RAIDERS_LOST_ARK_part_{0}.m4v".format(i)
         for striding in range(0, get_vid_length(video), STRIDE_LEN):
             f.write("{0},0,{1},{2}\n".format(video, striding, id))
