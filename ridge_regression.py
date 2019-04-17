c12d9f2b7beb3527092d1ea7d5a94a336bbb3b43
Cara Van Uden <Cara.E.Van.Uden.19@dartmouth.edu>
2019-04-17 13:20:12 -0400

    cleaned up code more and fixed HA mappers

--- ridge_regression.py
+++ ridge_regression.py
@@ -7,22 +7,25 @@ import mvpa2.suite as mv
 import numpy as np
 import pandas as pd
 from ridge import bootstrap_ridge
-import sys, os, time, csv
+import sys, os, time, csv, gzip
 
 VIDEO_DATA_DIR = '/ihome/cara/thesis/processed_raiders_videos/'
 RESPONSE_DATA_DIR = '/idata/DBIC/fma/id_studies/home/preprocess/final/raiders-32ch/'
-HYPERALIGNMENT_DIR = '/idata/DBIC/fma/id_studies/home/preprocess/aligned/raiders-32ch/'
+HYPERALIGN_DIR = '/idata/DBIC/fma/id_studies/home/preprocess/aligned/raiders-32ch/'
+RIDGE_RESULTS_DIR = '/ihome/cara/thesis/ridge_results/'
 
 SUBJECTS = ['sub-rid000005','sub-rid000011','sub-rid000014','sub-rid000015','sub-rid000020',\
             'sub-rid000028','sub-rid000029','sub-rid000033','sub-rid000038','sub-rid000042', 'sub-rid000043']
 HEMIS = ['lh', 'rh']
+RUNS = range(1,9)
 
 tr_movie = {1:369, 2:341, 3:372, 4:406}
 tr_fmri = {1:374, 2:346, 3:377, 4:412}
 TR = 2.5
 N_VERTICES = 40962
 N_MEDIAL = {'lh': 3486, 'rh': 3491}
-
+HYPERALIGN_MOVIE_HALF = {1: '5-6-7-8', 2: '5-6-7-8', 3: '5-6-7-8', 4: '5-6-7-8', \
+                         5: '1-2-3-4', 6: '1-2-3-4', 7: '1-2-3-4', 8: '1-2-3-4', }
 cortical_vertices = create_cortical_vertex_mask()
 
 # /idata/DBIC/fma/id_studies/home/preprocess/final/raiders-32ch/sub-rid000001/sub-rid000001_task-raiders_run-01_lh_freq32.npy
@@ -44,8 +47,11 @@ def get_full_surface(npy_data):
     return out
 
 # get ridge regression inputs
-# here are extracted CNN activations in response to video
-def get_stim(train_runs, test_run):
+# here are extracted CNN activations    0 in response to video
+def get_stim(test_runs):
+    train_runs = RUNS
+    train_runs.remove(test_run)
+
     # TODO: load CNN activations as np array
 
     stim = []
@@ -67,18 +73,26 @@ def get_stim(train_runs, test_run):
 
     return train_stim, test_stim
 
-def load_and_normalize_resp(subj_mapper, subj, run, hemi):
+def load_and_normalize_resp(subj, run, hemi):
     resp = np.load(os.path.join(RESPONSE_DATA_DIR, '{0}/{0}_task-raiders_run-0{1}_{2}_freq32.npy'.format(subj, run, hemi)))
 
+    # unzip hyperalignmemt mapper
+    filename = '{0}_{1}_qhyper-to-raiders-8ch_ico32_z_r20.0_sl-avg_reflection_non-scaling_non-norm-row_runs1-2-3-4.hdf5.gz'.format(subj, hemi, HYPERALIGN_MOVIE_HALF[run])
+    with gzip.open(filename, 'rb') as f:
+        mapper = mv.h5load(os.path.join(HYPERALIGN_DIR, f))
+
     mv.zscore(resp, chunks_attr=None)
-    test_resp = subj_mapper.forward(resp)
+    test_resp = mapper.forward(resp)
     resp = resp[:,cortical_vertices[hemi] == 1]
     mv.zscore(resp, chunks_attr=None)
 
     return resp
 
 # get the fMRI data
-def get_resp(test_subj, mappers, train_runs, test_run, hemi):
+def get_resp(test_subj, test_run, hemi):
+    train_runs = RUNS
+    train_runs.remove(test_run)
+
     train_subj = SUBJECTS[:].remove(test_subj)
     train_resp = []
 
@@ -100,40 +114,36 @@ def get_resp(test_subj, mappers, train_runs, test_run, hemi):
     return train_resp, test_resp
 
 test_run = sys.argv[1]
-train_runs = range(1,9)
-train_runs.remove(test_run)
-
 hemi = sys.argv[2]
 
-train_stim, test_stim = get_stim(test_run, train_runs)
-print('\nLoading hyperaligned mappers...')
-
-# sub-rid000021_rh_qhyper-to-raiders-8ch_ico32_z_r20.0_sl-avg_reflection_non-scaling_non-norm-row_runs5-6-7-8.hdf5.gz
-mappers = mv.h5load(os.path.join(HYPERALIGNMENT_DIR, 'search_hyper_mappers_life_mask_nofsel_{0}_leftout_{1}.hdf5'.format(hemi, test_run)))
+print('Getting video activations.')
+train_stim, test_stim = get_stim(test_run)
 
 for test_subj in SUBJECTS:
-    train_resp, test_resp = get_resp(test_subj, mappers, test_run, train_runs, hemi)
+    print('Getting fMRI data for test subj {0}.'.format(test_subj))
+    train_resp, test_resp = get_resp(test_subj, test_run, hemi)
 
     alphas = np.logspace(0, 3, 20)
-    nboots = len(train_runs)
+    nruns = len(RUNS) - 1
     chunklen = 15
     nchunks = 15
 
-    wt, corrs, alphas, bootstrap_corrs, valinds = bootstrap_ridge(train_stim, train_resp, test_stim, test_resp, alphas, nboots, single_alpha=True, return_wt=False)
+    print('Training bootstrap ridge regression.')
+    wt, corrs, alphas, _, _ = bootstrap_ridge(train_stim, train_resp, test_stim, test_resp, alphas, nruns, single_alpha=True, return_wt=False)
 
-    print('\nFinished training ridge regression, writing to file...')
-    directory = os.path.join('/dartfs-hpc/scratch/cara/models', '{0}/{1}/{2}/leftout_run_{3}'.format(align, model, stimfile, test_run), hemi, test_subj)
+    print('\nFinished training ridge regression, writing to file.')
+    directory = os.path.join(RIDGE_RESULTS_DIR, '{0}/{1}/{2}/'.format(hemi, test_run, test_subj))
     if not os.path.exists(directory):
         os.makedirs(directory)
 
     # no weights to save bc return_wt=False
-    # save the corrs
+    # save the corrs as npy and niml.dset
     np.save(os.path.join(directory, 'corrs.npy', corrs)
     out = get_full_surface(corrs)
     mv.niml.write(os.path.join(directory, 'corrs.{0}.niml.dset'.format(hemi)), out[None,:])
 
-    # save the alphas, don't need the bootstrap corrs and valinds
+    # save the alphas
     np.save(os.path.join(directory, 'alphas.npy'), alphas)
 
-    print('\nFinished writing to {0}.'.format(directory))
+    print('\nFinished writing corrs and alphas to {0}.'.format(directory))
 print('All done!')
